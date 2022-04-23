535b06b12e1376159dba6b88b9354db254a30065
valentin <valentinaless07@hotmail.com>
2022-04-23 12:51:42 -0300

    Add home title and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import Models from '../components/Models'
 import Nav from '../components/Nav'
 import styles from './styles/Home.module.css'
 
-const Home = () => {
+const Home = ({models, filterModels}) => {
   return (
     <>
         <Nav/>
-        <div className={styles.h1_container}>
+        <div className={styles.h1_models_container}>
         <h1>Descubrí todos los modelos</h1>
-        <Models/>
+        <Models models={models} filterModels={filterModels}/>
+        <footer className={styles.footer}></footer>
         </div>
     </>
   )
